7860289ae31e401b4a656dc90571b3368edcb862
KAkos27 <akoskosztolanyi@gmail.com>
2024-02-01 21:43:08 +0100

    random szamok

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-let i = 0;
+let spanIndex = 4;
 
-function szinez(gomb) { 
+function szinez(gomb) {
   var gombStilus = getComputedStyle(gomb);
   var gombSzin = gombStilus.backgroundColor;
-  document.getElementsByTagName("SPAN")[i].style.backgroundColor = gombSzin;
-  i++;
+  document.getElementsByTagName("SPAN")[spanIndex].style.backgroundColor =
+    gombSzin;
+  spanIndex++;
+}
+
+function feltolt() {
+  let gepTipp = new Array();
+  for (let i = 0; i < 4; i++) {
+    var velSzin = Math.floor(Math.random() * 6 + 1);
+    gepTipp[i] = velSzin;
+  }
+  for (let i = 0; i < gepTipp.length; i++) {
+    gepTipp[i];
+  }
 }
