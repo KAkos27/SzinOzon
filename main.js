fca4de70043e0344da443af45df47586e3a3def0
KAkos27 <akoskosztolanyi@gmail.com>
2024-02-03 21:54:26 +0100

    szinmukodes kesz

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 let spanIndex = 4;
 
-function szinez(gomb) {
-  let gombStilus = getComputedStyle(gomb);
-  let gombSzin = gombStilus.backgroundColor;
-  document.getElementsByTagName("SPAN")[spanIndex].style.backgroundColor =
-    gombSzin;
+const szinez = (classNev) => {
+  document.querySelectorAll("span")[spanIndex].classList.add(classNev);
   spanIndex++;
-}
+};
 
-function ujJatek() {
+const ujJatek = () => {
   spanIndex = 4;
-  let gepTipp = new Array();
+  let gepTipp = [];
   for (let i = 0; gepTipp.length != 4; i++) {
     let velSzin = Math.floor(Math.random() * 6 + 1);
     if (!gepTipp.includes(velSzin)) {
@@ -19,14 +16,41 @@ function ujJatek() {
   }
 
   for (let i = 0; i < gepTipp.length; i++) {
-    let gepStilus = document.getElementsByTagName("BUTTON");
-    let gepSzin = getComputedStyle(gepStilus[gepTipp[i]]).backgroundColor;
-    document.getElementsByTagName("SPAN")[i].style.backgroundColor = gepSzin;
+    document.querySelectorAll("span")[i].className = "";
+    const szinClass = document.querySelectorAll("button")[gepTipp[i]].className;
+    document.querySelectorAll("span")[i].classList.add(szinClass);
   }
 
-  for (let i = 4; i < 41; i++) {
-    let feherStilus = document.getElementsByTagName("BODY");
-    let feherSzin = getComputedStyle(feherStilus[0]).backgroundColor;
-    document.getElementsByTagName("SPAN")[i].style.backgroundColor = feherSzin;
-  }
-}
+  document
+    .querySelector(".tarolo")
+    .querySelectorAll("span")
+    .forEach((aktSpan) => {
+      aktSpan.className = "";
+    });
+  console.log(gepTipp);
+};
+
+// function szinez(gomb) {
+//   let gombStilus = getComputedStyle(gomb);
+//   let gombSzin = gombStilus.backgroundColor;
+//   document.getElementsByTagName("SPAN")[spanIndex].style.backgroundColor =
+//     gombSzin;
+//   spanIndex++;
+// }
+
+// function ujJatek() {
+//   spanIndex = 4;
+//   let gepTipp = new Array();
+//   for (let i = 0; gepTipp.length != 4; i++) {
+//     let velSzin = Math.floor(Math.random() * 6 + 1);
+//     if (!gepTipp.includes(velSzin)) {
+//       gepTipp.push(velSzin);
+//     }
+//   }
+
+//for (let i = 4; i < 41; i++) {
+//document.querySelectorAll('span')[i].className = "";
+// let feherStilus = document.getElementsByTagName("BODY");
+// let feherSzin = getComputedStyle(feherStilus[0]).backgroundColor;
+//document.getElementsByTagName("SPAN")[i].style.backgroundColor = feherSzin;
+//}
